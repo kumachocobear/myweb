0d916b7eb4e9e07b4f318d033fc736d6dce5a186
kumachocobear <kumachocobear@users.noreply.github.com>
2025-08-28 14:03:36 +0700

    Add HLS folder and files

--- video/urls.py
+++ video/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('stream/', views.stream_video, name='stream_video'), 
-]
+    path('', views.index, name='index'), 
