ae80f727653c4875d00853c2d8e8710cf1a3801d
kumachocobear <kumachocobear@users.noreply.github.com>
2025-08-28 14:19:20 +0700

    แก HLS

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8@rf^i6^0hod46+99%7mr+2@_j0a3&)3p(&oden1le1_1)rr_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = True
+
 ALLOWED_HOSTS = ['myweb-6zn6.onrender.com','127.0.0.1', 'localhost', 'yourusername.pythonanywhere.com']
 
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "video" / "templates"], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
